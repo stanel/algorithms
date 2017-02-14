27ece7e2a42b0f934d80fd1fad761450af960162
stefan <stefan@DESKTOP-3RQB798>
2017-02-14 11:31:49 +0100

    Add project files.

---
+++ Algorithms/Recursion/Fibonacci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Recursion
+{
+    public class Fibonacci
+    {
+        public int GetNumber(int position)
+        {
+            if (position <= 0)
+                return 0;
+
+            if (position == 1)
+                return 1;
+
+            return GetNumber(position-2) + GetNumber(position-1);
+        }
+    }
+}
---
+++ Algorithms/Recursion/FibonacciTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Recursion
+{
+    [TestClass]
+    public class FibonacciTest
+    {
+        [TestMethod]
+        public void FirstFibonacciNumberCanBeRetrieved()
+        {
+            AssertFibonacciNumberAtPositionIs(0, 0);
+        }
+
+        [TestMethod]
+        public void SecondFibonacciNumberCanBeRetrieved()
+        {
+            AssertFibonacciNumberAtPositionIs(1, 1);
+        }
+
+        [TestMethod]
+        public void ThirdFibonacciNumberCanBeRetrieved()
+        {
+            AssertFibonacciNumberAtPositionIs(1, 2);
+        }
+
+        [TestMethod]
+        public void FibonacciNumberAtPositionEightCanBeRetrieved()
+        {
+            AssertFibonacciNumberAtPositionIs(21, 8);
+        }
+
+        private void AssertFibonacciNumberAtPositionIs(int expectedNumber, int position)
+        {
+            Fibonacci fib = new Fibonacci();
+
+            int number = fib.GetNumber(position);
+
+            Assert.AreEqual(expectedNumber, number);
+
+        }
+    }
+}
